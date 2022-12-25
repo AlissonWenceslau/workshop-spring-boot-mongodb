c71a1911de8647df58a4dc714d6af9c18dc3f434
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2022-12-24 23:07:59 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/alissonwenceslau/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.alissonwenceslau.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alissonwenceslau.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
