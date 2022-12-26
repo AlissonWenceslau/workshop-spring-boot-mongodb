fe27637b291e1ffd865a966044ff4efc8d51bf30
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2022-12-26 14:16:13 -0300

    Comentários pertinentes na classe de configuração

--- src/main/java/com/alissonwenceslau/workshopmongo/config/Instantiation.java
+++ src/main/java/com/alissonwenceslau/workshopmongo/config/Instantiation.java
@@ -28,18 +28,22 @@ public class Instantiation implements CommandLineRunner {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
+		//Zerando o banco de dados
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 		
+		//Criando os usuários
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		//Criando os posts
 		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Partiu viagem", "Vou viajar para são paulo!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		//Associando os posts a um usuário
 		maria.getPost().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
 	}
