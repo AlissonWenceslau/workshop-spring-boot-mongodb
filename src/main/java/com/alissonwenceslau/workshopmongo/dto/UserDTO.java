7f59b36c76ac9f84c14c88ae5d4db07cd4dcf784
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2022-12-26 13:35:29 -0300

    Implementado serializable na classe UserDTO

--- src/main/java/com/alissonwenceslau/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/alissonwenceslau/workshopmongo/dto/UserDTO.java
@@ -1,8 +1,11 @@
 package com.alissonwenceslau.workshopmongo.dto;
 
+import java.io.Serializable;
+
 import com.alissonwenceslau.workshopmongo.domain.User;
 
-public class UserDTO {
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	private String id;
 	private String name;
